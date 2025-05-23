d8a74ffdf32db370c7d5ab0a700db62fd930c965
snowingfox <1503401882@qq.com>
2025-05-23 21:27:15 +0800

    feat: update golang

---
+++ html-to-markdown/go.mod
@@ -0,0 +1,9 @@
+module html-to-markdown
+
+go 1.24.3
+
+require (
+	github.com/JohannesKaufmann/dom v0.2.0 // indirect
+	github.com/JohannesKaufmann/html-to-markdown/v2 v2.3.2 // indirect
+	golang.org/x/net v0.40.0 // indirect
+)
---
+++ html-to-markdown/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"C"
+
+	htmltomarkdown "github.com/JohannesKaufmann/html-to-markdown/v2"
+)
+
+func ConvertHTMLToMarkdown(html *C.char) *C.char {
+	markdown, err := htmltomarkdown.ConvertString(C.GoString(html))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return C.CString(markdown)
+}
+
+func main() {
+
+}
+
