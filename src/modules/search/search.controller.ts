0c9aff01e4a726dd3b835cecdbbb10190ebdd8e1
snowingfox <1503401882@qq.com>
2025-05-23 20:41:42 +0800

    feat: add service inject

--- src/modules/search/search.controller.ts
+++ src/modules/search/search.controller.ts
@@ -1,10 +1,15 @@
 import { Public } from '@/common/decorator/public.decorator';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { SearchService } from './search.service';
 
 @Public()
 @Controller('search')
 export class SearchController {
+  @Inject()
+  private readonly service: SearchService;
+
   @Get('/google')
   async googleSearch() {
+    return this.service.googleSearch('nodejs');
   }
 }
